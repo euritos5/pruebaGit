448c77e47efc4e6f826ea8033b4018a257f9b6ed
Lorena <lorenabdominguez1982@gmail.com>
2019-04-17 11:54:07 -0300

    primer commit

---
+++ JavaApplication6caro/src/javaapplication6caro/Saludar.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplication6caro;
+
+/**
+ *
+ * @author programador
+ */
+public class Saludar {
+    public void unSaludo(){
+        System.out.println("Hola");
+    
+}
+}
